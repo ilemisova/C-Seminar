9f5c885dad13f34df05a200e8828831709755c01
ilemisova <ilemisova@mail.ru>
2022-06-30 00:21:58 +0300

    commits by Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,192 @@
+﻿/* 1задача определить является ли первое число значением квадратного корня второго
+int num1,num2,quad;
+
+Console.WriteLine("Inpute a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inpute a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num2 * num2;
+
+if(quad==num1)
+{
+Console.WriteLine("first number is quad of second number");
+}
+{
+Console.WriteLine("second number is not a quad of second number");
+}
+*/
+
+/* задача: Пользователь воодит одно число, и программа выводит квадрат этого числа
+int num1,quad;
+
+Console.WriteLine("Inpute a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+quad = num1 * num1;
+
+Console.WriteLine("Your number is " + quad);
+*/
+
+/* Задача: пользователь вводит число, программа выводит день недели
+
+int num1,num2,quad;
+
+Console.WriteLine("Inpute a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inpute a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+*/ 
+
+/*int num = 10;
+
+if(num == 1)
+{
+Console.WriteLine("Your day is Monday");
+}
+if(num == 2)
+{
+Console.WriteLine("Your day is Tuesday");
+}
+if(num == 3)
+{
+Console.WriteLine("Your day is Wednsday");
+}
+if(num == 4)
+{
+Console.WriteLine("Your day is Thursday");
+}
+if(num == 5)
+{
+Console.WriteLine("Your day is Friday");
+}
+if(num == 6)
+{
+Console.WriteLine("Your day is Saterday");
+}
+if(num == 7)
+{
+Console.WriteLine("Your day is Sunday");
+}
+if(num >= 8)
+{
+Console.WriteLine("Error day");}
+*/
+
+/*int n, current;
+
+Console.WriteLine("Input a number: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+current = n * (-1);
+
+while(current <= n)
+{
+	Console.WriteLine(current + " ");
+current++; // current = current +1
+}*/
+
+/*
+int day;
+Console.WriteLine("Input a day: ");
+day = Convert.ToInt32(Console.ReadLine());
+if(day == 1)
+{
+Console.WriteLine("Your day is Monday");
+}
+if(day == 2)
+{
+Console.WriteLine("Your day is Tuesday");
+}
+if(day == 3)
+{
+Console.WriteLine("Your day is Wednsday");
+}
+if(day == 4)
+{
+Console.WriteLine("Your day is Thursday");
+}
+if(day == 5)
+{
+Console.WriteLine("Your day is Friday");
+}
+if(day == 6)
+{
+Console.WriteLine("Your day is Saterday");
+}
+if(day == 7)
+{
+Console.WriteLine("Your day is Sunday");
+}
+if(day > 7 || day < 1)
+{
+Console.WriteLine("Error day");}*/
+
+
+/*int a,b,max;
+
+Console.WriteLine("Inpute a first number: ");
+a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inpute a second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+max = a;
+
+if(a > b)
+{
+Console.WriteLine("max " + a);
+}
+{
+Console.WriteLine("max " + b);
+}
+*/
+/*
+int a,b,c,max;
+Console.WriteLine("Inpute a first number: ");
+a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inpute a second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Inpute a third number: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+
+if(a >= b)
+	max = a;
+else
+	max = b;
+if	
+(c >= b)
+	max = c;
+		
+Console.WriteLine("max number is " + max);
+*/
+
+/*
+int N,current;
+
+Console.WriteLine("Input a number: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+current = (N / N)+1;
+
+while(current <= N)
+{
+	Console.WriteLine(current + " ");
+	current = current +2;
+}
+*/
+
+/*
+int N;
+
+Console.WriteLine("Input a number: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+	if 
+		(N % 2 == 0)
+	 	{Console.WriteLine( "четное" );}
+	else
+	 	{Console.WriteLine("нечетное" );}
+
+*/
+	
