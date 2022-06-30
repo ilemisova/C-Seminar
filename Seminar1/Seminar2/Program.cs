157af796bed94846e0b1900b22819d60edc4cc75
ilemisova <ilemisova@mail.ru>
2022-06-30 04:02:02 +0300

    task10 S2

---
+++ Seminar1/Seminar2/Program.cs
@@ -0,0 +1,16 @@
+﻿int CutNumber()
+{
+    int num = new Random().Next(100,1000);
+Console.WriteLine("Current random number is " + num);
+
+        int des1 = num / 10;
+        num = num /10;
+        int des = num % 10;
+        
+        
+    int result = des;
+return result;
+}
+
+int number = CutNumber();
+Console.WriteLine("Result number is " + number);
