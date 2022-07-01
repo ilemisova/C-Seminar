30c717b67795f00834f4675bdaa274e88491cb6e
ilemisova <ilemisova@mail.ru>
2022-07-01 04:12:06 +0300

    задачи с 3 семинара

--- Seminar1/Seminar2/Program.cs
+++ Seminar1/Seminar2/Program.cs
@@ -1,16 +1,31 @@
-﻿int CutNumber()
+﻿/*int CutNumber()
 {
     int num = new Random().Next(100,1000);
 Console.WriteLine("Current random number is " + num);
 
-        int des1 = num / 10;
-        num = num /10;
-        int des = num % 10;
-        
+        int des = (num % 100) / 10;             
         
     int result = des;
 return result;
 }
 
 int number = CutNumber();
-Console.WriteLine("Result number is " + number);
+Console.WriteLine("Result number is " + number);*/
+
+/*void CutNumberVoid()
+{
+    int num = new Random().Next(0,999);
+    Console.WriteLine("Current random number is " + num);
+       int arg1 = num /10;
+       int arg2 = num % 10; 
+    
+while(int num < 99)
+{
+  Console.WriteLine("Третье число " + arg2);  
+}
+if(int num > 100)
+    {
+    Console.WriteLine("Третьего числа нет");
+    };
+}
+CutNumberVoid();*/
---
+++ Seminar1/Seminar3/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Написать программу, которая принимает номер четверти 
+и выводит дипазон значений координат, возможных в этой 
+четверти:
+
+Метод "void"*/
+
+/*void ShowArea(int quart)
+{
+if(quart >= 1 && quart <=4)
+{
+if(quart == 1) Console.WriteLine("In thie quart x > 0 and y > 0");
+if(quart == 2) Console.WriteLine("In thie quart x < 0 and y > 0");
+if(quart == 3) Console.WriteLine("In thie quart x < 0 and y < 0");
+if(quart == 4) Console.WriteLine("In thie quart x > 0 and y > 0");
+}
+else
+Console.WriteLine("This number uncorrect");
+}
+
+Console.WriteLine("Input number of quart:" );
+int numQuart = Convert.ToInt32(Console.ReadLine());
+
+ShowArea(numQuart);*/
+
+
+/*Необходимо написать программу,которая принимает на вход координаты
+точки и в качестве результата выдает номер четверти, в которой
+эта точка находится.*/
+
+/*Метод "int Koord"
+
+int Koord(int a, int b)
+{
+	int result = -1;
+    	
+		if(a > 0 && b > 0) result = 1; 
+		if(a < 0 && b > 0) result = 2; 
+		if(a < 0 && b < 0) result = 3; 
+		if(a > 0 && b < 0) result = 4; 
+        return result;
+}
+
+Console.WriteLine("Input number of x: " );
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number of y: " );
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Koord(x, y));
+*/
