a1b9a0e2098bdd000548b9e7ff615b61dee019ae
ilemisova <ilemisova@mail.ru>
2022-07-06 21:50:06 +0300

    z1S4

--- Seminar1/Task1/Program.cs
+++ Seminar1/Task1/Program.cs
@@ -21,3 +21,24 @@ Console.WriteLine("max " + a);
 Console.WriteLine("max " + b);
 }
 */
+
+/* int FindSum;
+void PrintArray(int[] array)
+{
+	int count = array.Length;
+	
+	for (int i = 0; i < count; i++)
+	{
+		Console.Write($"{array[i]} ");
+	}
+	Console.WriteLine();
+}
+
+Console.WriteLine("Inpute a second m: ");
+m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum all numbers from A to {m} is {FindSum(m)}");
+
+
+PrintArray(arr);*/
+
