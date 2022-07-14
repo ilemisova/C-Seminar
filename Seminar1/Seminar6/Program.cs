5ffc9d8292d116ac194845121b83858b402f7e7e
ilemisova <ilemisova@mail.ru>
2022-07-14 20:00:58 +0300

    seminars6

--- Seminar1/Seminar6/Program.cs
+++ Seminar1/Seminar6/Program.cs
@@ -1,33 +1,13 @@
-﻿/*Напишите программу, которая перевернёт одномерный массив
- (последний элемент будет на первом месте, а первый - 
-на последнем и т.д.)*/
-
-/*int[] ReverveArray(int[] array)
+﻿void Triangle (int a, int b, int c)
 {
-	for(int i = 0; i< array.Length/2; i++)
-	{
-	int temp = arra[i];
-	array[i] = array[array.Lehgth - 1 - i];
-	array[array.Lehgth - 1 - i] = temp;
-	}
-	return array;
-}*/
-/*
-int[] myArray = {3, 5, 1, 8, 2, 9, 4};
-myArray = ReserveArray(myArray);
-
-for(int i = 0; i < myArray.Length; i++)
-Console.Write(myArray[i] + " ");
-*/
-
-string ChangeDigitSystem(int num)
-{
-	string resultNumber = string.Empty;
-
-	while(num > 0)
-	{
-		resultNumber = num % 2 + resultNumber;
-	}
-    return resultNumber;
+	if (a < b +c && b < a + c && c < a + b) Console.WriteLine(" ok ");
+    else  Console.WriteLine( "No");
 }
-Console.WriteLine(ChangeDigitSystem(18));
+Console.WriteLine("Input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input num3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+Triangle(num1, num2, num3);
