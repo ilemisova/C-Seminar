90bb1975b8077305cb73ea6a6c31bc873abe82f1
ilemisova <ilemisova@mail.ru>
2022-07-18 11:11:38 +0300

    task

---
+++ HW6_7/H7/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] CreateRandom2dArray(double rows, double columns)
+{
+	double[,] newArray = new double[rows, columns];
+       	for(double i = 0; i < rows; i++)
+	    for(double j = 1; j < columns; j++);
+            
+	    newArray[i,j] = new Random().NextDouble();
+	return newArray;
+}
+
+void Show2dArray(double[,] array)
+{
+ 	for(double i = 0; i < array.GetLength(0); i++)
+	{
+	 	for(double j = 0; j < array.GetLength(1); j++)
+	Console.Write(array[i,j]);
+
+	Console.WriteLine();
+	}
+}
+
+Console.WriteLine("Input number of rows: ");
+double m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+double n = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+
+
--- HW6_7/HW7/Program.cs
+++ HW6_7/HW7/Program.cs
@@ -1,10 +1,44 @@
-﻿Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
-8 7,8 -7,1 9
+8 7,8 -7,1 9*/
+/*
+int[,] CreateRandom2dArray(int rows, int columns, double minValue, double maxValue)
+{
+	int[,] newArray = int[rows, columns];
+    	for(int i = 0; i < rows; i++)
+		for(int j= 0; j < columns; j++)
+			newArray[i,j] = new Random().Next(minValue, maxValue +1);
+	return newArray;
+}
 
-Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+void Show2dArray(int[,] array)
+{
+ 	for(int i =0; i < array.GetLength(0); i++)
+	{
+	 	for(int j=0; j< array.GetLength(1); j++)
+	Console.Write(array[i,j] + " ");
+
+	Console.WriteLine();
+	}
+}
+
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input maxpossible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = CreateRandom2dArray(m,n,min,max);
+Show2dArray(myArray);*/
+
+
+
+/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 Например, задан массив:
 
 1 4 7 2
@@ -19,3 +53,4 @@ m = 3, n = 4.
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+*/
