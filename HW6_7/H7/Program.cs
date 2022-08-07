50990fc7b263d47e7fec364767769adf9fc56175
ilemisova <ilemisova@mail.ru>
2022-08-07 17:19:31 +0300

    67

--- HW6_7/H7/Program.cs
+++ HW6_7/H7/Program.cs
@@ -29,59 +29,8 @@ Console.WriteLine("Input number of columns: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 double[,] array = CreateRandom2dArray(m,n);
-Show2dArray(array);*/
+Show2dArray(array); */
 
-/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
-
-/*int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
-{
-	int[,] newArray = new int[rows, columns];
-
-	for(int i = 0; i < rows; i++)
-		for(int j= 1; j < rows; j++)
-			newArray[i,j] = new Random().Next(minValue, maxValue +1);
-
-	return newArray;
-}
-int[,] Sum2dArray(int rows, int columns)
-{
-	int[,] newArray = new int[rows, columns];
-	sum = 0;
-	for(int i = 0; i < rows; i++)
-		for(int j= 1; j < rows; j++)
-			sum += sum / j;
-
-	return sum;
-}
-void Show2dArray(int[,] array)
-{
- 	for(int i =0; i < array.GetLength(0); i++)
-	{
-	
- 	for(int j=0; j< array.GetLength(1); j++)
-		Console.Write(array[i,j] + " ");
-
-	Console.WriteLine();
-	}
-}
-
-Console.WriteLine("Input number of rows: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input number of columns: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input min possible value: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input maxpossible value: ");
-int max = Convert.ToInt32(Console.ReadLine());
-
-int[,] myArray = CreateRandom2dArray(m,n,min,max);
-Show2dArray(myArray);
-*/
 
 /*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 и возвращает значение этого элемента или же указание, что такого элемента нет.
@@ -98,28 +47,24 @@ int[,] CreateRandom2dArray(int rows, int columns)
 	for(int i = 0; i < rows; i++)
 		for(int j= 1; j < rows; j++)
 			newArray[i,j] = new Random().Next(1,25);
-
-	return newArray;
-}
-int[,]CreateArray(int position1, int position2)
-{
-	int[] newArray = new int[size];
-
-	Console.WriteLine("Creating array here: ");
-	for(int i = 0; i < size; i++)
-	for 
-     if (position1 < 0 | posititon1 > array.GetLength(0) - 1 | position2 < 0 | position2 > array.GetLength(1) - 1)
-        {
-            Console.WriteLine($"Элемент не существует");
-        }
-        else
-        {
-            Console.WriteLine($"Значение элемента массива = {0}", array[position1, position2]);
-        }
-        Console.WriteLine();
-        return array[position1, position2];
+		return newArray;
 }
 	
+void FindPosition (int[,] array)
+{
+int m = 0, n = 0;
+
+for(int i = 0; i < array.GetLength(0); i++)
+	{
+		for(int j = 0; j < array.GetLength(1); j++)
+			if(m > array.GetLength(0) && n > array.GetLength(1))
+				Console.WriteLine($"Элемент не существует в массиве");
+			if(m < array.GetLength(0) && n < array.GetLength(1));
+				array[i,j] = array[m,n];
+				Console.WriteLine($"Значение элемента: "+ array[i,j] );
+	}
+}
+    	
 	void Show2dArray(int[,] array)
 {
  	for(int i =0; i < array.GetLength(0); i++)
@@ -133,13 +78,14 @@ int[,]CreateArray(int position1, int position2)
 }
 
 Console.WriteLine("Input number of rows: ");
-int m = Convert.ToInt32(Console.ReadLine());
+int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number of columns: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int columns = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input pos1: ");
-int position1 = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input pos2: ");
-int position2 = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+
 
 int[,] myArray = CreateRandom2dArray(m,n);
 Show2dArray(myArray);
